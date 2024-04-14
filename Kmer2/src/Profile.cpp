ce8fcd20354a04a2d79b2d819033dcad4e1d194f
eladio106 <eladio106@correo.ugr.es>
2024-04-14 14:04:23 +0200

    KMER2 1.1
    Version finalizada, faltan pequeños errores de compilación pero lista para ser entregada

--- Kmer2/src/Profile.cpp
+++ Kmer2/src/Profile.cpp
@@ -78,7 +78,7 @@ static const int Profile::getCapacity(){
 int Profile::findKmer(Kmer kmer, int initialPos, int finalPos){
     int resultado=-1;
     for (int i = initialPos; i <= finalPos; ++i) {
-        if (this->_vectorKmerFreq.getKmer().toString() == kmer.toString()) {
+        if (this->_vectorKmerFreq[i].getKmer().toString() == kmer.toString()) {
             resultado=i;
             i=finalPos;
         }
@@ -91,7 +91,7 @@ int Profile::findKmer(Kmer kmer){
     for (int i = 0; i <= this->_vectorKmerFreq[0].getKmer().size()-1 ; ++i) {
         if (this->_vectorKmerFreq[i].getKmer() == kmer.toString()) {
             resultado=i;
-            i=finalPos;
+            i=this->_vectorKmerFreq[0].getKmer().size()-1;
         }
     }
     return resultado;
@@ -113,4 +113,103 @@ void Profile::sort(){
             swap(&_vectorKmerFreq[i], _size, i, i+1);
         }
     }
+}
+
+void Profile::save(const char &fileName[]) const{
+    
+    ofstream file(fileName[]);
+    
+    if(!file.is_open)
+        throw std::ios_base::failure
+    
+    fileName[0]<< this->_profileId;
+    
+    for(int i=1; i<=this->_size;i++){
+        
+        fileName[i] << _vectorKmerFreq[i-1];
+    }
+    
+    file.close();
+}
+
+
+void Profile::load(char &fileName[]){
+    
+    ifstream file(fileName[]);
+    
+    if(!file.is_open)
+        throw std::ios_base::failure;
+                
+    if(_size>fileName[] || fileName[]<0){
+        throw out_of_range(string("Profile(int _size): invalid capacity"));
+    }
+    
+    if (fileName != MAGIC_STRING_T){
+        throw invalid_argument(string("Profile(string MAGIC_STRING_T): invalid magic string"));
+    }
+    
+    
+    getline(fileName[0], _profileId);
+    
+    for(int i=1; i<this->_size;i++){
+        
+        getline(fileName[i],_vectorKmerFreq[i-1]);
+    }
+    
+    file.close();
+}
+
+void Profile::normalize(string validNucleotides){
+    
+    for(int i=0; i<_size; i++){
+        for(int j=0; j<_vectorKmerFreq[i].getKmer().size();j++){
+            _vectorKmerFreq[i].getKmer().toUpper();
+            if(! IsValidNucleotide(_vectorKmerFreq[i].getKmer().at(j),validNucleotides)){
+                _vectorKmerFreq[i].getKmer().at(j) = _vectorKmerFreq[i].getKmer().MISSING_NUCLEOTIDE;
+            }    
+        }
+    }
+    
+    for(int i=0; i<_size; i++){//Compara si hay Kmers iguales
+        for(int j= i+1; j<_size; j++){
+            if(_vectorKmerFreq[i].getKmer().toString() == _vectorKmerFreq[i].getKmer().toString()){
+                _vectorKmerFreq[i].setFrequency(_vectorKmerFreq[i].getFrequency() + _vectorKmerFreq[j].getFrequency());
+                deletePos(j);
+            }
+        }
+    }    
+}
+
+void Profile::deletePos(int pos){
+    
+    for(int i=pos; i<_size-1; i++){
+        _vectorKmerFreq[i]=_vectorKmerFreq[i+1];
+    }
+    _vectorKmerFreq[nElements-1].setFrequency(0);
+    _vectorKmerFreq[_size-1].setKmer(_vectorKmerFreq[_size-1].getKmer().MISSING_NUCLEOTIDE);
+    _size--;
+}
+
+void Profile::zip(bool deleteMissing=false, int lowerBound=0){
+    
+    
+    for(int i=0; i<_size-1; i++){
+        if(_vectorKmerFreq[i].getFrequency()== lowerBound) deleteMissing=true;
+        for(int j=0; j<_vectorKmerFreq[i].getKmer().size(); j++){
+            if(_vectorKmerFreq[i].getKmer().at(j)== _vectorKmerFreq[i].getKmer().MISSING_NUCLEOTIDE) deleteMissing=true;
+        }
+        
+        if(deleteMissing){
+            for(int k=i; k<_size-1; k++){
+                _vectorKmerFreq[k]=_vectorKmerFreq[k+1];
+            }      
+            
+            _vectorKmerFreq[_size-1].setFrequency(0);
+            _vectorKmerFreq[_size-1].setKmer(_vectorKmerFreq[_size-1].getKmer().MISSING_NUCLEOTIDE);
+            _size--;
+            
+            deleteMissing=false;
+        }
+    }
+    
 }
